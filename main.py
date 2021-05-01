fd1a647a92c13afde1961c1637b0719acb77b76b
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2021-05-01 16:08:01 -0400

    main py

--- main.py
+++ main.py
@@ -11,24 +11,3 @@ class Perceptron(object):
         self.Iterations = Iterations
         self.errors = []
         self.weights = np.zeros(1 + x.shape[1])
-    
-    # Defining fit method for model training.
-    def fit(self, x, y):
-        self.weights = np.zeros(1 + x.shape[1])
-        for i in range(self.Iterations):
-            error = 0
-            for xi, target in zip(x, y):
-                update = self.learn_rate * (target - self.predict(xi))
-                self.weights[1:] += update*xi
-                self.weights[0] += update
-                error += int(update != 0)
-            self.errors.append(error)
-        return self
-    
-    # Net Input method for summing the given matrix inputs and their corresponding weights.
-    def net_input(self, x):
-        return np.dot(x, self.weights[1:]) + self.weights[0]
-    
-    # Predict method for predicting the classification of data inputs.
-    def predict(self, x):
-        return np.where(self.net_input(x) >= 0.0, 1, -1)

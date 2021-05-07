33c76f17147be24e5e9f7c9dbc4e0d8e3a61f02a
Yash D <60755759+dhayalytcnj@users.noreply.github.com>
2021-05-07 19:02:50 -0400

    edited to include w0 weight for constant x0 = 1

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 #Start class
 class Perceptron:
-    def __init__(self, eta=0.01, epoch=200):
+    def __init__(self, eta=0.4, epoch=200):
         #epoch - int number of passes over the training dataset.
         self.eta = eta
         self.epoch = epoch
@@ -15,25 +15,27 @@ class Perceptron:
 
 
     def dotProductOutput(self, training_vector): # Return o value (1 or -1) after dot prodcut
-        o = 1 + numpy.dot(training_vector, self.w[0:])  # add w0 to the equation instead of using in dot prod. w0 is represented as a constant 1
-        return numpy.where(o >= 0.0, 1, -1)             # returns 1s for every o >= 0, else -1 
+        o = self.w[0] + numpy.dot(training_vector, self.w[1:])  # add w0 to the equation instead of using in dot prod. w0 is represented as a constant 1
+        return numpy.where( o >= 0.0, 1, -1 )             # returns 1s for every o >= 0, else -1 
 
 
     def ptr(self, training_vector, target_values, weight_type):
         #training_vector - array of training vectors of the number of examples and the number of flower measurements.
         #target_values - array of target values.
+        #weight_type - choosing initial weights
 
+        # 5 weights, because constant x0 = 1, and it also gets a weight w0.
         if weight_type == 0:
-            self.w = [0, 0, 0, 0]                           # w - initialize weights to all 0s
+            self.w = [0, 0, 0, 0, 0]                           # w - initialize weights to all 0s
 
         elif weight_type == 1:
-            self.w = [1, 1, 1, 1]                           # w - initialize weights to all 1s
+            self.w = [1, 1, 1, 1, 1]                           # w - initialize weights to all 1s
 
         elif weight_type == 2:
-            self.w = [0.1, 0.2, 0.3, 0.4]                   # w - initialize weights to 4 different numbers between 0 to 1
+            self.w = [0.1, 0.2, 0.3, 0.4, 0.5]                   # w - initialize weights to 4 different numbers between 0 to 1
 
         elif weight_type == 3:
-            self.w = [0, 0, 0, 0]
+            self.w = [0, 0, 0, 0, 0]
             for i in self.w:
                 self.w[i] = round(np.random.random(), 2)    # w - initialize weights to 4 random numbers between 0 to 1
 
@@ -42,7 +44,8 @@ class Perceptron:
             errors = 0
             for xi, target in zip(training_vector, target_values):          # for each tuple in (training vector, target values) tuples
                 deltaW = self.eta * (target - self.dotProductOutput(xi))    # represent delta delta_w = eta * (t-o)
-                self.w[0:] += (deltaW * xi)                                 # delta_wi = delta_w * xi. Thus leading to: wi = wi + delta_wi
+                self.w[1:] += (deltaW * xi)                                 # delta_wi = delta_w * xi. Thus leading to: wi = wi + delta_wi
+                self.w[0:] += deltaW                                        # updating weight for constant x0.
 
                 if deltaW != 0.0:                                           # if delta w has no change, then right match found
                     errors += 1
@@ -53,9 +56,15 @@ class Perceptron:
                 break
             
             # Brian FIX THIS CONDITION
-            if i > 50    # if error count wont hit 0, check if the last 5 values are the same
+            if i > 50:    # if error count wont hit 0, check if the last 3 values are the same
+                ele = self.errorList[i][0]
+                count = 0
+                for x in range(3):
+                    if self.errorList[i-x][0] == ele:
+                        count+= 1
+                if count == 3:
+                    break
                  
-            
         return self
 #End class
 
@@ -82,7 +91,7 @@ for i in lp1.errorList.keys():
 lp2_target = numpy.where(name == 'Iris-versicolor', 1, -1) 
 lp2 = p.ptr(df_measures, lp2_target, 0)
 
-print ("\n*** LP 3 ***")
+print ("\n*** LP 2 ***")
 for i in lp2.errorList.keys():
     print("Epoch " + str(i) + ": " + str(p.errorList[i][0]) + " error(s).    Weight: " + str(p.errorList[i][1]) )
 
@@ -94,4 +103,3 @@ lp3 = p.ptr(df_measures, lp3_target, 0)
 print ("\n*** LP 3 ***")
 for i in lp3.errorList.keys():
     print("Epoch " + str(i) + ": " + str(p.errorList[i][0]) + " error(s).    Weight: " + str(p.errorList[i][1]) )
-print (lp3.errorList[99 - 5][0])
